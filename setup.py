cd24a3624d054ee1513be61487e94a9cad068369
deathstroke111 <moresridatt@gmail.com>
2023-09-30 10:11:05 +0530

    fresh leaf

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='testing for mlops',
+    author='deathstrke1001',
+    license='MIT',
+)
